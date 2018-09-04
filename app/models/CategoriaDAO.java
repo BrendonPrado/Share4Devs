5dfdf6382492cd57c6a5d719ee38840610ecefff
BrendonPrado <brendonmatheus123@gmail.com>
2018-09-04 08:15:24 -0300

    teste

--- app/models/CategoriaDAO.java
+++ app/models/CategoriaDAO.java
@@ -1,4 +1,31 @@
 package models;
 
+
+import com.avaje.ebean.Ebean;
+import com.avaje.ebean.EbeanServer;
+
 public class CategoriaDAO {
+    public void Insert(Categoria categoria){
+        categoria.save();
+    }
+    public Categoria SelectPorID(int id){
+        EbeanServer ebeans = Ebean.getDefaultServer();
+        Categoria categoria = ebeans.find(Categoria.class).where().like("id", String.valueOf(id)).findUnique();
+        return categoria;
+    }
+
+    public Categoria SelectPorNome(String nome){
+        EbeanServer ebeans = Ebean.getDefaultServer();
+        Categoria categoria = ebeans.find(Categoria.class).where().like("nome", nome).findUnique();
+        return categoria;
+    }
+
+    public void  UpdateCategoria(Categoria usuario){
+        usuario.update();
+    }
+
+    public void Delete(Categoria usuario){
+        usuario.delete();
+    }
+
 }
